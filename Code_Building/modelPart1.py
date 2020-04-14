81cb37fe86b2cea74ef21883782cf46859480e2c
Aayush Sureshchander <asaayush@gmail.com>
2020-04-14 10:44:07 -0400

    Major Update --> First KNN Model tested. 79% accuracy with only form details --> Able to generate necessary data from the readingDatabases.py file --> Moving on to more techniques

--- Code_Building/modelPart1.py
+++ Code_Building/modelPart1.py
@@ -8,19 +8,41 @@
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
+import pandas
+import matplotlib.pyplot as plt
+from mlxtend.plotting import plot_decision_regions
 
 # Import Required Database
-winPredictData = 'winPredictData.csv'                    # Yet to finalize
-[X, y] = winPredictData                                  # X represents all the features, y represents the actual W/L/D
+winPredictData = pandas.read_csv('winPredictData.csv')                    # Yet to finalize
+y = winPredictData.iloc[:,8]                                  # X represents all the features, y represents the actual W/L/D
+
+X = winPredictData.iloc[:,6:8]
+
+temp = winPredictData.loc[winPredictData['7']=='W']
+W_xaxis = temp['5']            # X Axis will represent home form
+W_yaxis = temp['6']            # Y Axis will represent away form
+temp = winPredictData.loc[winPredictData['7']=='D']
+D_xaxis = temp['5']            # X Axis will represent home form
+D_yaxis = temp['6']            # Y Axis will represent away form
+temp = winPredictData.loc[winPredictData['7']=='L']
+L_xaxis = temp['5']            # X Axis will represent home form
+L_yaxis = temp['6']            # Y Axis will represent away form
 
 # K-Nearest Neighbors
-winPredictKNNModel = KNeighborsClassifier(n_neighbors=3)
+winPredictKNNModel = KNeighborsClassifier(n_neighbors=25)
 # Split Data into Training and Testing
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3)
 winPredictKNNModel.fit(X_train,y_train)             # Training the model
 y_predict = winPredictKNNModel.predict(X_test)      # Predicting W/L/D
 # Checking accuracy
 score = metrics.accuracy_score(y_test,y_predict)
+print(score)
+
+# matplot-lib
+x = [X,y].values
+plot_decision_regions(X,y,clf=winPredictKNNModel,legend=2)
+plt.plot(W_xaxis,W_yaxis,'r+',D_xaxis,D_yaxis,'bx',L_xaxis,L_yaxis,'g*')
+plt.show()
 
 
 # Support Vector Machines
--- Code_Building/readingDatabases.py
+++ Code_Building/readingDatabases.py
@@ -11,6 +11,7 @@ countryData_file = 'CountryData.csv'
 leagueData_file = 'LeagueData.csv'
 matchData_file = 'MatchData.csv'
 extraData_file = 'MatchData4_ExtraData_NeedsCleaning.csv'
+teamData_file = 'TeamData.csv'
 
 # We could import using any of the following strategies:
 # 1. Using direct CSV readers
@@ -28,6 +29,8 @@ tags = ['id','country_id','league_id','season','stage','date','match_api_id','ho
 matchData = pandas.read_csv(matchData_file, names=tags)
 tags = ['goal','shoton','shotoff','foulcommit','card','cross','corner','possession']
 extraData = pandas.read_csv(extraData_file, names=tags)
+tags = ['id','team_api_id','team_fifa_api_id','team_long_name','team_short_name']
+teamData = pandas.read_csv(teamData_file, names=tags)
 
 # Important point to note: As it turns out, the first line of the csv is the tags.
 # I should remove it after writing sufficient code to handle it. UPDATE: I can do this!
@@ -37,6 +40,7 @@ countryData = countryData.iloc[1:,:]
 leagueData = leagueData.iloc[1:,:]
 matchData = matchData.iloc[1:,:]
 extraData = extraData.iloc[1:,:]
+teamData = teamData.iloc[1:,:]
 
 # Just checking on the shape of the data
 # print(countryData.shape)
@@ -48,7 +52,7 @@ extraData = extraData.dropna()
 size = extraData.shape
 size = size[0]
 goalID = []
-i=0
+i = 0
 while i < size:
     temp = str(extraData.iloc[i, 0])
     goalID_temp = re.findall('<id>([0-9]+)',temp)
@@ -60,58 +64,117 @@ while i < size:
 # The data has been pre-formatted in Excel to be in an order we can exploit
 # In any continuous 'x' stages of the game, if 'W' is repeating (counter), good form
 # I would like to ideally add a 'form score', that will act as a useful feature
-teamList = ['10000'] # ,'8342']
-seasonList = ['2009/2010'] # ,'2010/2011']
-countryID = '1'
 
-for seasonID in seasonList:
-    seasonMatches = matchData.loc[(matchData['season'] == seasonID) & (matchData['country_id'] == countryID)]
-    for teamID in teamList:
-        # Let us identify general form
-        genMatches = seasonMatches.loc[(seasonMatches['home_team_api_id'] == teamID)|
-                                       (seasonMatches['away_team_api_id'] == teamID)]
-        genForm = pandas.DataFrame(genMatches['stage'])
-        #print(genForm)
-        counter = 1; prevState = 0; newState = 1
-        lastValue = 0
-        stageList = genMatches['stage']
-        for stage in stageList:
-            temp = genMatches.loc[(genMatches['stage'] == stage)]
-            kkk = (temp.loc[temp['away_team_api_id'] == teamID]['away_team_api_id'] == teamID).empty
-            homeGoal = int(temp.loc[temp['stage'] == stage, 'home_team_goal'].item())
-            awayGoal = int(temp.loc[temp['stage'] == stage, 'away_team_goal'].item())
-            print(stage, homeGoal,awayGoal)
-            if kkk:    # Home Matches
-                print('Home Match')
-                if homeGoal > awayGoal:
-                    print('Home Win')
-                    # form[counter] = lastValue + 1*(newState+1)     # Home Win
-                elif homeGoal == awayGoal:
-                    print('Home Draw')
-                    # form[counter] = lastValue + 0                  # Home Draw
-                elif homeGoal < awayGoal:
-                    print('Home Loss')
-                    # form[counter] = lastValue + -1                 # Home Loss
-                    #newState = 0
-            else:   # Away Matches
-                print('Away Match')
-                if homeGoal < awayGoal:
-                    print('Win')
-                    #form[counter] = lastValue + 1*(newState+1)     # Away Win
-                elif homeGoal == awayGoal:
-                    print('Draw')
-                    #form[counter] = lastValue + 0                  # Away Draw
-                elif homeGoal > awayGoal:
-                    print('Loss')
-                    #form[counter] = lastValue + -1                 # Away Loss
-            counter += 1
-        #print(form)
-        # if homeMatches[]
-            # formCol = 1
+yearList = matchData['season'].drop_duplicates()
+teamList = teamData['team_api_id']
+seasonList = yearList
+countryID = countryData['id']
+
+lastX = 3
+data = {('0', '0', '0', '0', '0')}
+formTable = pandas.DataFrame(data)
+
+'''
+for countryId in countryID:
+    for seasonID in seasonList:
+        seasonMatches = matchData.loc[(matchData['season'] == seasonID) & (matchData['country_id'] == countryId)]
+        for teamID in teamList:
+            form = []
+            formPoints = []
+            genMatches = seasonMatches.loc[(seasonMatches['home_team_api_id'] == teamID)|
+                                           (seasonMatches['away_team_api_id'] == teamID)]
+            stageList = genMatches['stage']
+            for stage in stageList:
+                #print(countryId, seasonID, teamID,stage)
+                temp = genMatches.loc[(genMatches['stage'] == stage)]
+                kkk = (temp.loc[temp['away_team_api_id'] == teamID]['away_team_api_id'] == teamID).empty
+                homeGoal = int(temp.loc[temp['stage'] == stage, 'home_team_goal'].item())
+                awayGoal = int(temp.loc[temp['stage'] == stage, 'away_team_goal'].item())
+                if kkk:                                 # Home Matches
+                    if homeGoal > awayGoal:
+                        formPoints.append(1)                  # Home Win
+                    elif homeGoal == awayGoal:
+                        formPoints.append(0)                  # Home Draw
+                    elif homeGoal < awayGoal:
+                        formPoints.append(-1)                 # Home Loss
+                else:                                   # Away Matches
+                    if homeGoal < awayGoal:
+                        formPoints.append(1)                  # Away Win
+                    elif homeGoal == awayGoal:
+                        formPoints.append(0)                  # Away Draw
+                    elif homeGoal > awayGoal:
+                        formPoints.append(-1)                 # Away Loss
+            for i in range(0,lastX):
+                form.append(0)
+            for i in range(lastX,len(formPoints)):
+                temp2 = 0
+                for j in range(0,lastX):
+                    temp2 += (lastX - j+1)*formPoints[i-j]
+                form.append(temp2)
+            for i in range(len(formPoints)):
+                if formPoints[i] == 1:
+                    formPoints[i] = 'W'
+                elif formPoints[i] == 0:
+                    formPoints[i] = 'D'
+                else:
+                    formPoints[i] = 'L'
+
+            # We need to create a form table
+            j = len(form)
+            k = 0
+            for i in stageList:
+                d = {(seasonID, i, teamID, form[k],formPoints[k])}
+                d = pandas.DataFrame(data=d)
+                formTable = pandas.concat([formTable, d],ignore_index=True)
+                k = k+1
+                if k == j:
+                    break
+formTable = formTable.iloc[1:,:]
+formTable.columns = ['season', 'stage', 'team_id', 'form', 'result']
+formTable.to_csv('formTable.csv')
+'''
+formTable = pandas.read_csv('formTable.csv')
+print(formTable.shape)
+
+# Now that we have the form data, we have to finally form the data that we will be using; winPredictData.csv
+
+# winPredictData = pandas.DataFrame({('0','0','0','0','0','0','0','0')})
+
+# FEATURES 1:
+# league, season, stage, home_team_id, away_team_id, home_form, away_form, result
+# (league, season, stage, home_team_id, away_team_id) can be taken from matchData
+# FEATURES 2:
+# features 1 + home_team_goal, away_team_goal
+i = 0
+print(len(matchData))
+homeTeamForm = pandas.DataFrame()
+awayTeamForm = pandas.DataFrame()
+matchResult = pandas.DataFrame()
+for i in range(len(matchData)):
+    season = matchData.iloc[i,:]['season']
+    stage = matchData.iloc[i,:]['stage']
+    homeTeam = matchData.iloc[i, :]['home_team_api_id']
+    awayTeam = matchData.iloc[i, :]['away_team_api_id']
+    temp = formTable.loc[(formTable['season']==season) & (formTable['stage']==int(stage))]
+    hForm = temp.loc[temp['team_id'] == int(homeTeam)]['form']
+    result = temp.loc[temp['team_id'] == int(homeTeam)]['result']
+    aForm = temp.loc[temp['team_id'] == int(awayTeam)]['form']
+    homeTeamForm = pandas.concat([homeTeamForm, hForm],ignore_index=True)
+    awayTeamForm = pandas.concat([awayTeamForm, aForm], ignore_index=True)
+    matchResult = pandas.concat([matchResult, result], ignore_index=True)
+
+# data = {(matchData['league_id'],seasons,stages, homeTeams, awayTeams, )}
+# print(homeTeamForm,awayTeamForm)
 
 
-customDF = pandas.DataFrame(matchData['league_id'])
-#print(customDF)
+#print(matchData['league_id'], matchData['season'], matchData['stage'], matchData['home_team_api_id'],
+     #             matchData['away_team_api_id'] , homeTeamForm, awayTeamForm, matchResult)
+league = matchData['league_id']
 
+winPredictData = pandas.concat([league, matchData['season'], matchData['stage'],
+                                matchData['home_team_api_id'], matchData['away_team_api_id']
+                                   , homeTeamForm, awayTeamForm, matchResult], axis=1,ignore_index=True)
 
-# Let us continue
+print(winPredictData)
+winPredictData.to_csv('winPredictData.csv')
+
